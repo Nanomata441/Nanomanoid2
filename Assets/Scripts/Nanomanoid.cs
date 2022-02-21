9da9ed9833d445cb29ace297144fea457c60c633
Yuhi <kamilantos588@gmail.com>
2022-02-21 19:06:11 +0100

    poprawki

--- Assets/Scripts/Nanomanoid.cs
+++ Assets/Scripts/Nanomanoid.cs
@@ -4,57 +4,43 @@ using UnityEngine;
 
 public class Nanomanoid : MonoBehaviour
 {
-    public Sprite mainSprite;
     public float movementSpeed;
-    public SpriteRenderer spriteRenderer;
-    public void Movement()
-    
-      
-    {
-        spriteRenderer.sprite = mainSprite;
-        if (Input.GetKey(KeyCode.W)) 
 
+    public Sprite mainSprite;
+
+    public SpriteRenderer spriteRenderer;
+
+    private void Update()
+    {
+        Movement();
+    }
+    public void Movement()
+    {
+       
+        Vector2 newPosition; // nowa pozycja
+        if (Input.GetKey(KeyCode.W))
         {
-            movementSpeed = 5;
-            Vector2 newPosition; // nowa pozycja
-            
             newPosition.x = transform.position.x;
             newPosition.y = transform.position.y + movementSpeed * Time.deltaTime;
             transform.position = newPosition;
-            
-            
-
-
         }
         if (Input.GetKey(KeyCode.S))
         {
-            movementSpeed = 5;
-            Vector2 newPosition;
             newPosition.x = transform.position.x;
             newPosition.y = transform.position.y - movementSpeed * Time.deltaTime;
-            transform.position = newPosition;
+            transform.position = newPosition; spriteRenderer.sprite = mainSprite;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            movementSpeed = 5;
-            Vector2 newPosition;
             newPosition.x = transform.position.x - movementSpeed * Time.deltaTime; ;
             newPosition.y = transform.position.y;
             transform.position = newPosition;
-
-
         }
         if (Input.GetKey(KeyCode.D))
         {
-            movementSpeed = 5;
-            Vector2 newPosition;
             newPosition.x = transform.position.x + movementSpeed * Time.deltaTime;
             newPosition.y = transform.position.y;
             transform.position = newPosition;
-
-
         }
     }
-    }
-
-
+}
